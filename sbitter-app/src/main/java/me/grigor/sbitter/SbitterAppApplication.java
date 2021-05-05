84003515779c24b375ec329378d92c125de0d051
grigs <grigsogood96@gmail.com>
2021-05-05 19:21:08 +0300

    Initial commit Add generated springboot module

---
+++ sbitter-app/src/main/java/me/grigor/sbitter/SbitterAppApplication.java
@@ -0,0 +1,13 @@
+package me.grigor.sbitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbitterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbitterAppApplication.class, args);
+	}
+
+}
---
+++ sbitter-app/src/test/java/me/grigor/sbitter/SbitterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.grigor.sbitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbitterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
