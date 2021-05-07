444f283abc257a858774442e8ead6ba84fa09c08
grigs <grigsogood96@gmail.com>
2021-05-07 21:24:01 +0300

    Add pictures

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -1,8 +1,13 @@
 import React from "react"
+import sberLogo from "./sber.jpg"
 
 function MainPage() {
   return (
-    <h1> Sbitter frontend application</h1>
+      <>
+        <h1> Sbitter frontend application</h1>
+        <img src={sberLogo} className="sber-logo" alt="sber-logo"/>
+      </>
+
   );
 }
 
